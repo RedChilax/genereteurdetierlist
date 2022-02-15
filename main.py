f31c9cd73e41e4678c58111386ba0b26a8e70141
RedChilax <74028533+RedChilax@users.noreply.github.com>
2022-02-15 01:07:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,85 +1,73 @@
 import random
-from turtle import width
 from PIL import Image , ImageDraw , ImageFont
-tier_number = random.randint(2,6)
+import os
+
+NB_USER = 53
+OFFSET = 1
+
+tierNb = random.randint(2,6)
 colorlist = [(255,127,127) , (255,191,127), (255,223,127) , (255,255,127) , (191,255,127) , (127,255,237)]
-tier_name = []
+tiersName = []
 
-def random_line(fname):
+def random_line(fname, nbLines):
     lines = open(fname).read().splitlines()
-    return random.choice(lines)
-xx= 0
-while xx < tier_number:
-    tier_name.append(random_line('liste_francais.txt'))
-    xx += 1
+    return random.sample(lines, nbLines)
 
-im = Image.open("templat.png")
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
+
+tiersName = random_line('liste_francais.txt', tierNb)
+
+im = Image.new(mode = "RGB", size = (2000, 2000), color = (26, 26, 23))
 fnt = ImageFont.truetype('fonts/arial.ttf', 30)
 d = ImageDraw.Draw(im)
 
-users_tier = {}
-i = 1
-while i <= 53:
-    users_tier[i] = random.randint(1 , tier_number)
-    i+=1
+tiers = [[] for i in range(tierNb)]
+for i in range(NB_USER):
+    tierValue = random.randint(0 , tierNb-1)
+    tiers[tierValue].append(i)
 
-tier_counter = {}
-h = 1
-while h <= tier_number:
-    count = 0
-    for key in users_tier:
-        if(users_tier[key] == h):
-            count+=1
-    tier_counter[h] = count
-    h+=1
-print(tier_counter)
-x = 0 
+#print(tiers)
+
 xpos1 = 0
 ypos1 = 0
 xpos2 = 225
-ypos2 = 185
-while(x < tier_number):
-    size = int(tier_counter[x+1]/11)
-    print(size)
-    t = 0
-    while t < size:
-        ypos2 += 185
-        t += 1
-    d.rectangle((xpos1 , ypos1 , xpos2 , ypos2), fill=colorlist[x], outline='black' , width=2)
-    d.text((20,ypos1+50), tier_name[x], font=fnt ,fill=(0,0,0))
-    t = 0
-    while t < size:
-        ypos1 += 185
-        t += 1
-    ypos1 += 185
-    ypos2 += 185
-    x+=1
+ypos2 = 150
 
-g = 1
-newposy=20
-while g <= tier_number:
-    newposx=235
-    compteur = 0
-    for key in users_tier:
-        if users_tier[key] == g:
-            compteur +=1
-            if(compteur % 11 == 0):
-                newposy += 185
-                newposx =235
 
-            img2 = Image.open("users/"+str(key)+".png")
-            img2 = img2.resize((150,150), Image.ANTIALIAS)
-            im.paste(img2 , (newposx,newposy))
-            
-            
-            newposx+=160
-    newposy += 185
-    g+=1
+for tierIndex in range(tierNb):
+    size = int(len(tiers[tierIndex])/12)
+    #print(size)
 
-img3 = Image.open("tiermaker-logo.png")
-img3.putalpha(200)
-img3 = img3.resize((400,73), Image.ANTIALIAS)
-im.paste(img3 , (2000-400,0))
+    ypos2 += size * 150 
+
+    d.rectangle((xpos1 , ypos1 , xpos2 , ypos2), fill=colorlist[tierIndex], outline='black' , width=2)
+    d.text((20,ypos1+50), tiersName[tierIndex], font=fnt ,fill=(0,0,0))
+
+    cpt = 0
+    usrPosx=226
+    usrPosy=ypos1
+    for user in tiers[tierIndex]:
+
+        usrImg = Image.open("users/"+str(user+1)+".png")
+        usrImg = usrImg.resize((150,150), Image.ANTIALIAS)
+        im.paste(usrImg , (usrPosx,usrPosy + OFFSET))        
+        usrPosx+=150
+        
+        cpt += 1
+        if(cpt % 11 == 0):
+            usrPosy += 150
+            usrPosx = 235
+    
+    
+    ypos1 = ypos2
+    ypos2 += 150
+    
+
+logoImg = Image.open("tiermaker-logo.png")
+logoImg.putalpha(200)
+logoImg = logoImg.resize((400,73), Image.ANTIALIAS)
+im.paste(logoImg , (2000-400,0))
 
 im.save("tierlist_generated.png")
-
+im.show()
